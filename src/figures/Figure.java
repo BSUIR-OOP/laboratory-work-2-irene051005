e201283132ec69c0cfef3f82aa9e7d5fbbdd43f2
Irina Scherbakova <irina.ivanovna.sa@gmail.com>
2022-06-02 15:52:54 +0300

    Laba files

---
+++ src/figures/Figure.java
@@ -0,0 +1,22 @@
+package figures;
+
+import java.util.ArrayList;
+
+public abstract class Figure implements Cloneable {
+    public String FigureName;
+    public ArrayList<Point> points;
+
+    public Figure() {
+        points = new ArrayList<>();
+        this.FigureName = getClass().getSimpleName();
+    }
+
+    @Override
+    public Figure clone() throws CloneNotSupportedException {
+        Figure newFigure = (Figure) super.clone();
+        newFigure.points = (ArrayList<Point>) this.points.clone();
+        return newFigure;
+    }
+
+    public abstract void AddPoints(Point a, Point b);
+}
